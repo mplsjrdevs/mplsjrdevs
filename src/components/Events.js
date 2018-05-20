e6dc0738aa3dda0e32fb17b31b3cf71f11fe3916
Matt Decuir <decuirm@gmail.com>
2018-05-20 10:06:11 -0500

    Finish styling and filling in events

--- src/components/Events.js
+++ src/components/Events.js
@@ -7,32 +7,164 @@ const Events = () => {
       <hr />
       <h3>Upcoming</h3>
       <p>
-        Tues, June 12th<br />
-        Intro to DNS, by{' '}
+        <span className="date">June 12, 2018</span>
+        <br />
+        <span className="text">
+          Intro to DNS, by{' '}
+          <a
+            href="https://twitter.com/justincardinal"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Justin Cardinal
+          </a>
+        </span>
+      </p>
+      <hr />
+      <h3>
         <a
-          href="https://twitter.com/justincardinal"
+          href="https://mplsjrdevs.eventbrite.com#past_events"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Justin Cardinal
+          Past Events
         </a>
-      </p>
-      <hr />
-      <h3>Past Events</h3>
+      </h3>
       <ul>
-        <li>May 8, 2018: Intro to Functional Programming, by Joe Karlsson</li>
-        <li>Apr 14, 2018: Minnebar Session</li>
         <li>
-          Mar 13, 2018: Diversity & Inclusion in Tech, by Ian Coldwater & Matt
-          Decuir
+          <span className="date">May 8, 2018</span>
+          <br />
+          <span className="text">
+            Intro to Functional Programming, by{' '}
+            <a
+              href="https://twitter.com/joekarlsson1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Joe Karlsson
+            </a>
+          </span>
         </li>
-        <li>Feb 13, 2018: Intro to Git, by Joshua Wehner</li>
         <li>
-          Jan 9, 2018: A Recruiter&#39;s Perspective on the Jr Dev Job Search,
-          by Paul DeBettignies
+          <span className="date">Apr 14, 2018</span>
+          <br />
+          <span className="text">Minnebar Session</span>
         </li>
-        <li>Dec 12, 2017: Coding Exercises, by Morgan Jones & Ben Tilkens</li>
-        <li>Nov 14, 2017: Intro to TDD, by Eryn O&#39;Neil</li>
+        <li>
+          <span className="date">Mar 13, 2018</span>
+          <br />
+          <span className="text">
+            Diversity & Inclusion in Tech, by{' '}
+            <a
+              href="https://twitter.com/iancoldwater"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Ian Coldwater
+            </a>
+            {' & '}
+            <a
+              href="https://twitter.com/experimatt"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Matt Decuir
+            </a>
+          </span>
+        </li>
+        <li>
+          <span className="date">Feb 13, 2018</span>
+          <br />
+          <span className="text">
+            Intro to Git, by{' '}
+            <a
+              href="https://twitter.com/jaw6"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Joshua Wehner
+            </a>
+          </span>
+        </li>
+        <li>
+          <span className="date">Jan 9, 2018</span>
+          <br />
+          <span className="text">
+            Jr Dev Job Search Tips, by{' '}
+            <a
+              href="https://twitter.com/mnheadhunter"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Paul DeBettignies
+            </a>
+          </span>
+        </li>
+        <li>
+          <span className="date">Dec 12, 2017</span>
+          <br />
+          <span className="text">
+            Coding Exercises, by Morgan Jones & Ben Tilkens
+          </span>
+        </li>
+        <li>
+          <span className="date">Nov 14, 2017</span>
+          <br />
+          <span className="text">
+            Intro to TDD, by{' '}
+            <a
+              href="https://twitter.com/eryno"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Eryn O&#39;Neil
+            </a>
+          </span>
+        </li>
+        <li>
+          <span className="date">Sep 12, 2017</span>
+          <br />
+          <span className="text">
+            Intro to Infosec, by{' '}
+            <a
+              href="https://twitter.com/iancoldwater"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Ian Coldwater
+            </a>
+            {' & '}
+            <a
+              href="https://twitter.com/nightmarejs"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Kat Traxler
+            </a>
+          </span>
+        </li>
+        <li>
+          <span className="date">Aug 2, 2017</span>
+          <br />
+          <span className="text">Show & Tell at Pryes Brewing</span>
+        </li>
+        <li>
+          <span className="date">Jun 1, 2017</span>
+          <br />
+          <span className="text">Lightning Talks at Software for Good</span>
+        </li>
+        <li>
+          <span className="date">Apr 20, 2017</span>
+          <br />
+          <span className="text">
+            Jr Dev Introductions at Lake Monster Brewing
+          </span>
+        </li>
+        <li>
+          <span className="date">Mar 25, 2017</span>
+          <br />
+          <span className="text">Minnebar Session</span>
+        </li>
       </ul>
     </section>
   );
