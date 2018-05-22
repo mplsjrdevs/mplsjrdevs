8eb104b4ef421da2613696565aad80bd2b185d1b
Matt Decuir <decuirm@gmail.com>
2018-05-21 21:45:24 -0500

    Style Code of Conduct and update content

--- src/components/CodeOfConduct.js
+++ src/components/CodeOfConduct.js
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button, Dialog, Classes, Intent } from '@blueprintjs/core';
+import CoCContent from './CoCContent';
 
-const CodeOfConduct = () => {
-  return (
-    <section id="code-of-conduct">
-      <h2>Code of Conduct</h2>
-      <p>
-        We are dedicated to providing an open, welcoming, and inclusive
-        community.<br />
-        <br />
-        As such, we have a <a href="/coc">code of conduct</a> that we expect all
-        of our members to follow.
-      </p>
-    </section>
-  );
-};
+class CodeOfConduct extends Component {
+  state = { isModalOpen: false };
+
+  render() {
+    return (
+      <section id="code-of-conduct">
+        <h2>Code of Conduct</h2>
+        <p className="padded">
+          We are dedicated to providing an open, welcoming, and inclusive
+          community.
+          <br />
+          <br />
+          As such, we have a code of conduct that we expect all of our members
+          to follow.
+        </p>
+        <Button
+          text="View Code of Conduct"
+          className="CodeofConduct button"
+          onClick={this.toggleModal}
+        />
+        <Dialog
+          icon="document"
+          isOpen={this.state.isModalOpen}
+          onClose={this.toggleModal}
+          title="Code of Conduct"
+          className="CodeofConduct modal"
+        >
+          <CoCContent />
+          <div className={Classes.DIALOG_FOOTER}>
+            <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+              <Button
+                text="Close"
+                intent={Intent.PRIMARY}
+                onClick={this.toggleModal}
+                className="CodeofConduct button"
+              />
+            </div>
+          </div>
+        </Dialog>
+      </section>
+    );
+  }
+
+  toggleModal = () => {
+    this.setState({ ...this.state, isModalOpen: !this.state.isModalOpen });
+  };
+}
 
 export default CodeOfConduct;
