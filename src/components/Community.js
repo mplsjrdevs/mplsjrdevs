58129fe852f699d3a103f9ba16c715b55d4aa4fb
Matt Decuir <matt.decuir@govdelivery.com>
2018-06-01 13:14:33 -0500

    Add faces to the community section

--- src/components/Community.js
+++ src/components/Community.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import jenessa from '../images/community/jenessa-white.jpg';
+import chelsey from '../images/community/chelsey-mckinney.jpg';
+import garret from '../images/community/garret-saarinen.jpg';
+import matt from '../images/community/matt-decuir.jpg';
 
 const Community = () => {
   return (
     <section id="community">
       <h2>Community</h2>
-      <p>
-        Meet our members!<br />
-        <br />
-        (Coming soon...)
+      <p className="community-images">
+        <img src={chelsey} alt="Chelsey McKinney" className="community-image" />
+        <img src={garret} alt="Garret Saarinen" className="community-image" />
+        <img src={jenessa} alt="Jenessa White" className="community-image" />
+        <img src={matt} alt="Matt Decuir" className="community-image" />
       </p>
     </section>
   );
