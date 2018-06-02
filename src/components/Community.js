67bd00719e014d4e1c27c1fcfa46a95efd8f3b72
Matt Decuir <decuirm@gmail.com>
2018-06-02 09:48:08 -0500

    Add placeholder for carousel

--- src/components/Community.js
+++ src/components/Community.js
@@ -8,12 +8,25 @@ const Community = () => {
   return (
     <section id="community">
       <h2>Community</h2>
-      <p className="community-images">
-        <img src={chelsey} alt="Chelsey McKinney" className="community-image" />
-        <img src={garret} alt="Garret Saarinen" className="community-image" />
-        <img src={jenessa} alt="Jenessa White" className="community-image" />
-        <img src={matt} alt="Matt Decuir" className="community-image" />
-      </p>
+      <div className="flex-container">
+        <div className="carousel">
+          <img
+            src={jenessa}
+            alt="bio goes here eventually"
+            className="responsive"
+          />
+        </div>
+        <div className="community-images">
+          <img
+            src={chelsey}
+            alt="Chelsey McKinney"
+            className="community-image"
+          />
+          <img src={garret} alt="Garret Saarinen" className="community-image" />
+          <img src={jenessa} alt="Jenessa White" className="community-image" />
+          <img src={matt} alt="Matt Decuir" className="community-image" />
+        </div>
+      </div>
     </section>
   );
 };
