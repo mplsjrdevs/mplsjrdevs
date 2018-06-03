11ac108bf6563f02f49a502a3b5868567b34ba69
Matt Decuir <decuirm@gmail.com>
2018-06-03 13:26:38 -0500

    add community members data structure, make clicking on faces work

--- src/components/Community.js
+++ src/components/Community.js
@@ -1,95 +1,84 @@
-import React from 'react';
-import jenessa from '../images/community/jenessa-white.jpg';
-import chelsey from '../images/community/chelsey-mckinney.jpg';
-import garret from '../images/community/garret-saarinen.jpg';
-import matt from '../images/community/matt-decuir.jpg';
-import sarah from '../images/community/sarah-cooke.jpg';
-import kam from '../images/community/kam-kubesh.jpg';
-import brandon from '../images/community/brandon-johnson.jpg';
-import ian from '../images/community/ian-coldwater.jpg';
+import React, { Component } from 'react';
+import communityMembers from '../static/communityMembers';
+import _ from 'lodash';
 
-const Community = () => {
-  let communityMembers = [
-    {
-      name: 'Jenessa White',
-      image: '../images/community/jenessa-white.jpg',
-      bio:
-        "Hi I'm Jenessa. I'm a Software Engineer at Software For Good in Minneapolis, MN.",
-      links: [
-        {
-          title: 'GitHub',
-          url: 'https://github.com/jenessawhite',
-          icon: 'fa-github'
-        },
-        {
-          title: 'LinkedIn',
-          url: 'https://www.linkedin.com/in/jenessawhite/',
-          icon: 'fa-linkedin-square'
-        },
-        {
-          title: 'Twitter',
-          url: 'https://twitter.com/JNessView',
-          icon: 'fa-twitter'
-        }
-      ]
-    },
-    {
-      name: 'Matt Decuir',
-      image: '../images/community/matt-decuir.jpg',
-      bio: "Hi I'm Matt. I'm a Software Engineer at Granicus in St. Paul, MN.",
-      links: [
-        {
-          title: 'GitHub',
-          url: 'https://github.com/experimatt',
-          icon: 'fa-github'
-        },
-        {
-          title: 'LinkedIn',
-          url: 'https://www.linkedin.com/in/mattdecuir/',
-          icon: 'fa-linkedin-square'
-        },
-        {
-          title: 'Twitter',
-          url: 'https://twitter.com/experimatt',
-          icon: 'fa-twitter'
-        }
-      ]
+class Community extends Component {
+  // randomly select a community member to feature by default
+  state = { featuredPerson: _.sample(communityMembers) };
+
+  componentWillMount() {
+    // parse personId from URL
+    let personId = this.getPersonIdFromURL();
+
+    // try to find featured person if present
+    let featuredPerson = personId && _.find(communityMembers, ['id', personId]);
+
+    if (featuredPerson) {
+      this.setState({ featuredPerson: featuredPerson });
     }
-  ];
+  }
 
-  return (
-    <section id="community">
-      <h2>Community</h2>
-      <div className="flex-container">
-        <div className="carousel">
-          <img
-            src={jenessa}
-            alt="bio goes here eventually"
-            className="responsive left community-image"
-          />
-          {communityMembers[0]['bio']}
+  componentDidMount() {
+    // scroll to community section if user navigated directly to a community URL
+    if (this.getPersonIdFromURL()) {
+      // wait until document is loaded before scrolling
+      if (
+        window.document &&
+        typeof window.document.createElement === 'function'
+      ) {
+        window.document.getElementById('community').scrollIntoView(true);
+      }
+    }
+  }
+
+  getPersonIdFromURL = () => {
+    // split URL pathname into an array
+    let path = window.location.pathname.split('/');
+
+    // return second element in path if first element is 'community'
+    return path[1] === 'community' ? path[2] : null;
+  };
+
+  render() {
+    return (
+      <section id="community">
+        <h2>Community</h2>
+        <div className="flex-container">
+          <div className="carousel">
+            <img
+              src={this.state.featuredPerson['image']}
+              alt={this.state.featuredPerson['name']}
+              className="responsive left"
+            />
+            <div className="bio">{this.state.featuredPerson['bio']}</div>
+          </div>
+          <div className="community-images">
+            {communityMembers.map(person => {
+              return (
+                <a
+                  key={person.id}
+                  href={`community/${person.id}`}
+                  onClick={e => this.handleImageClick(e, person)}
+                >
+                  <img
+                    src={person['image']}
+                    alt={person['name']}
+                    className="community-image"
+                  />
+                </a>
+              );
+            })}
+          </div>
         </div>
-        <div className="community-images">
-          <img
-            src={chelsey}
-            alt="Chelsey McKinney"
-            className="community-image"
-          />
-          <img src={garret} alt="Garret Saarinen" className="community-image" />
-          <img src={jenessa} alt="Jenessa White" className="community-image" />
-          <img src={matt} alt="Matt Decuir" className="community-image" />
-          <img src={sarah} alt="Sarah Cooke" className="community-image" />
-          <img src={kam} alt="Kam Kubesh" className="community-image" />
-          <img
-            src={require('../images/community/brandon-johnson.jpg')}
-            alt="Brandon Johnson"
-            className="community-image"
-          />
-          <img src={ian} alt="Ian Coldwater" className="community-image" />
-        </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    );
+  }
+
+  handleImageClick = (e, person) => {
+    e.preventDefault();
+    this.setState({ ...this.state, featuredPerson: person });
+    window.history.pushState(null, '', `/community/${person.id}`);
+  };
+}
 
 export default Community;
---
+++ src/static/communityMembers.js
@@ -0,0 +1,91 @@
+export default [
+  {
+    id: 'chelsey-mckinney',
+    name: 'Chelsey McKinney',
+    image: require('../images/community/chelsey-mckinney.jpg'),
+    bio: "Hi I'm Chelsey.",
+    links: []
+  },
+  {
+    id: 'garret-saarinen',
+    name: 'Garret Saarinen',
+    image: require('../images/community/garret-saarinen.jpg'),
+    bio: "Hi I'm Garret.",
+    links: []
+  },
+  {
+    id: 'jenessa-white',
+    name: 'Jenessa White',
+    image: require('../images/community/jenessa-white.jpg'),
+    bio: "Hi I'm Jenessa.",
+    links: [
+      {
+        title: 'GitHub',
+        url: 'https://github.com/jenessawhite',
+        icon: 'fa-github'
+      },
+      {
+        title: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/jenessawhite/',
+        icon: 'fa-linkedin-square'
+      },
+      {
+        title: 'Twitter',
+        url: 'https://twitter.com/JNessView',
+        icon: 'fa-twitter'
+      }
+    ]
+  },
+  {
+    id: 'matt-decuir',
+    name: 'Matt Decuir',
+    image: require('../images/community/matt-decuir.jpg'),
+    bio:
+      "Hi I'm Matt. I'm a Software Engineer and one of the organizers of Mpls Jr Devs.",
+    links: [
+      {
+        title: 'GitHub',
+        url: 'https://github.com/experimatt',
+        icon: 'fa-github'
+      },
+      {
+        title: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/mattdecuir/',
+        icon: 'fa-linkedin-square'
+      },
+      {
+        title: 'Twitter',
+        url: 'https://twitter.com/experimatt',
+        icon: 'fa-twitter'
+      }
+    ]
+  },
+  {
+    id: 'sarah-cooke',
+    name: 'Sarah Cooke',
+    image: require('../images/community/sarah-cooke.jpg'),
+    bio: "Hi I'm Sarah.",
+    links: []
+  },
+  {
+    id: 'kam-kubesh',
+    name: 'Kam Kubesh',
+    image: require('../images/community/kam-kubesh.jpg'),
+    bio: "Hi I'm Kam.",
+    links: []
+  },
+  {
+    id: 'brandon-johnson',
+    name: 'Brandon Johnson',
+    image: require('../images/community/brandon-johnson.jpg'),
+    bio: "Hi I'm Brandon.",
+    links: []
+  },
+  {
+    id: 'ian-coldwater',
+    name: 'Ian Coldwater',
+    image: require('../images/community/ian-coldwater.jpg'),
+    bio: "Hi I'm Ian.",
+    links: []
+  }
+];
