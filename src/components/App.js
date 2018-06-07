47e09ca2c1e48f8be4725b0049ed7264092c7ba0
Matt Decuir <decuirm@gmail.com>
2018-06-06 22:26:30 -0500

    Remove unnecessary comments/styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
 import ReactGA from 'react-ga';
-// import fontawesome from '@fortawesome/fontawesome';
-// import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-// import faRegular from '@fortawesome/fontawesome-free-regular';
-// import faSolid from '@fortawesome/fontawesome-free-solid';
-// import faBrands from '@fortawesome/fontawesome-free-brands';
 
 import Header from './Header';
 import Welcome from './Welcome';
@@ -14,8 +9,6 @@ import Community from './Community';
 import CodeOfConduct from './CodeOfConduct';
 import Contact from './Contact';
 import '../styles/App.css';
-
-// fontawesome.library.add(faRegular, faSolid, faBrands);
 
 class App extends Component {
   componentWillMount() {
