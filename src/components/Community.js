379d0cfec449614abd4b3d43a3754630a4d076cd
Matt Decuir <decuirm@gmail.com>
2018-06-06 22:16:14 -0500

    Make the carousel work

--- src/components/Community.js
+++ src/components/Community.js
@@ -3,44 +3,34 @@ import communityMembers from '../static/communityMembers';
 import _ from 'lodash';
 
 class Community extends Component {
-  // randomly select a community member to feature by default
+  // randomly choose a community member to feature
   state = { featuredPerson: _.sample(communityMembers) };
 
   componentWillMount() {
-    // parse personId from URL
-    let personId = this.getPersonIdFromURL();
-
-    // try to find featured person if present
+    // set featuredPerson if present
+    let personId = this.parsePersonIdFromURL();
     let featuredPerson = personId && _.find(communityMembers, ['id', personId]);
 
     if (featuredPerson) {
       this.setState({ featuredPerson: featuredPerson });
+    } else {
+      // rotate featured person every 10 seconds
+      this.intervalId = setInterval(() => {
+        this.setState({ featuredPerson: _.sample(communityMembers) });
+      }, 10 * 1000);
     }
   }
 
   componentDidMount() {
-    // scroll to community section if user navigated directly to a community URL
-    if (this.getPersonIdFromURL()) {
-      // wait until document is loaded before scrolling
-      if (
-        window.document &&
-        typeof window.document.createElement === 'function'
-      ) {
-        window.document.getElementById('community').scrollIntoView(true);
-      }
-    }
+    // scroll to community section if on a community URL
+    this.parsePersonIdFromURL() && this.scrollTo('community');
   }
 
-  getPersonIdFromURL = () => {
-    // split URL pathname into an array
-    let path = window.location.pathname.split('/');
-
-    // return second part of path if first part is 'community'
-    return path[1] === 'community' ? path[2] : null;
-  };
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
 
   render() {
-    let FeaturedBio = this.state.featuredPerson['bio'];
     return (
       <section id="community">
         <h2>Community</h2>
@@ -51,7 +41,7 @@ class Community extends Component {
               alt={this.state.featuredPerson['name']}
               className="responsive center"
             />
-            {this.state.featuredPerson['bio'] && <FeaturedBio />}
+            {this.componentizedBio(this.state.featuredPerson)}
           </div>
           <div className="images-container">
             {communityMembers.map(person => {
@@ -81,9 +71,37 @@ class Community extends Component {
     e.preventDefault();
     this.setState({ ...this.state, featuredPerson: person });
     window.history.pushState(null, '', `/community/${person.id}`);
+    this.scrollTo('community');
+    clearInterval(this.intervalId);
+  };
+
+  parsePersonIdFromURL = () => {
+    // parse pathname and return personId if on a community URL
+    let pathName = window.location.pathname.split('/');
+    return pathName[1] === 'community' ? pathName[2] : null;
+  };
+
+  scrollTo(location) {
+    // wait until document is loaded before scrolling
+    if (
+      window.document &&
+      typeof window.document.createElement === 'function'
+    ) {
+      window.document.getElementById(location).scrollIntoView(true);
+    }
+  }
+
+  componentizedBio = person => {
+    if (person['bio']) {
+      // import bio component if present and return it
+      let FeaturedBio = person['bio'];
+      return <FeaturedBio />;
+    } else {
+      // otherwise, return "Hi, I'm person"
+      let firstName = person['name'].slice(0, person['name'].indexOf(' '));
+      return <div>{`Hi, I'm ${firstName}.`}</div>;
+    }
   };
 }
 
 export default Community;
-
-// <div className="bio" dangerouslySetInnerHTML={{ __html: this.state.featuredPerson['bio']}}></div>
