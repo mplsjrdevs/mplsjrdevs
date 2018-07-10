073d92871854e6226040a2175b646f658b8be176
Sarah Cooke <marshcooke1@gmail.com>
2018-07-09 22:01:19 -0500

    updated bio

--- src/components/community/sarah-cooke.js
+++ src/components/community/sarah-cooke.js
@@ -5,6 +5,14 @@ export default {
   website: 'http://example.com',
   github: 'https://github.com/username',
   linkedin: 'https://www.linkedin.com/in/sarahmcooke/',
-  bio: () =>
-    'Prior to entering the tech industry, Sarah spent four years working in arts administration. Through various responsibilities she developed an interest in computer programming and attended Prime Digital Academy. </br> Since graduation, Sarah has joined Kipsu in a technical support engineer role. Beyond technology, Sarah plays bass trombone and enjoys running. You can keep up with her on LinkedIn and Twitter.'
+  bio: () => (
+    <div>
+      Hey, I&#39;m Sarah.
+      <br />
+      <br />
+      I started out as self-taught but graduated from Prime Digital Academy. I
+      currently work as a technical support engineer, and am an organizer of
+      MplsJrDevs.
+    </div>
+  )
 };
