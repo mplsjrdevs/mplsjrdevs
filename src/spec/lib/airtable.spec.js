14a87dfe882c5b7dc7486fb9e043fac94edecdae
Brandon Johnson <b@johnsonmn.org>
2019-01-26 22:23:27 -0600

    Refactor slightly and add specs to make me feel slightly better

---
+++ src/spec/lib/airtable.spec.js
@@ -0,0 +1,26 @@
+import { createEvent, listEvents } from '../../lib/airtable';
+import eventData from '../../static/eventData';
+
+const DEMO_ID = 'demo_id';
+
+const mockClient = {
+  get: path =>
+    Promise.resolve({
+      data: eventData.map((d, i) => ({ fields: d, id: `${DEMO_ID}_${i}` }))
+    }),
+  post: (path, data) => Promise.resolve({ data: { fields: data, id: DEMO_ID } })
+};
+
+it('can create an event', async () => {
+  const res = await createEvent(mockClient)({});
+  expect(res.data).toBeDefined();
+  expect(res.data.id).toBeDefined();
+  expect(res.data.fields).toBeDefined();
+});
+
+it('can list events', async () => {
+  const res = await listEvents(mockClient)(eventData[0]);
+  expect(res.data).toBeDefined();
+  expect(res.data).toBeInstanceOf(Array);
+  expect(res.data[0].fields).toBeDefined();
+});
