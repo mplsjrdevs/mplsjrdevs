d04816cbd552d7b4f216069f619365673fe67696
ntceleste <nathanielceleste@gmail.com>
2020-05-26 11:39:24 -0500

    bio and image

---
+++ src/components/community/nate-celeste.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default {
+  id: 'nate-celeste',
+  name: 'Nate Celeste',
+  twitter: '',
+  github: '',
+  linkedin: '',
+  bio: () => (
+    <div>
+      Hi, I&#39;m Nate!
+      <br />
+      <br />
+      I graduated from Case Western Reserve University (it's in Cleveland, OH)
+      in 2019 with a BA in Computer Science. I couldn't wait to get back to Mpls
+      and start my journey as a developer with the awesome tech community here!
+      I'm currently an organizer for Mpls Jr Devs.
+      <br />
+      <br />
+      My tech passions include IoT, Mobile Development, and AR!
+    </div>
+  )
+};
